bd168e6107e4e3b60f2a9c939970ee518fc0342e
Corey Frang <gnarf37@gmail.com>
2017-07-13 12:32:52 -0400

    Add tests and a few features

--- src/index.js
+++ src/index.js
@@ -1,25 +1,81 @@
+const loaderUtils = require("loader-utils");
 const htmlparser = require("htmlparser");
+const defaultOptions = {
+  cssModules: true,
+  mangleIds: true,
+  idPattern: 'svg-[name]-[sha512:hash:base64:7]',
+};
 
-function assembleNode(node) {
+
+const getOptions = context => Object.assign({},
+  loaderUtils.getOptions(context),
+  defaultOptions
+);
+
+function getIdReplacement(context, options, id) {
+  return loaderUtils.interpolateName(context, options.idPattern, { content: id }).replace(/[^a-zA-Z0-9]/g,'-');
+}
+
+function assembleNode(loaderContext, node, root) {
     if (node.type === 'text') {
-        return `'${node.data.replace(/\n/g, '\\n')}'`;
+        return JSON.stringify(node.data)
     }
 
-    const attribs = JSON.stringify(node.attribs || {});
+    const options = getOptions(loaderContext);
+
+    let useAttribs = Object.assign({}, node.attribs || {});
+    if (options.mangleIds) {
+      Object.keys(useAttribs).forEach(key => {
+        if (key === 'id') {
+          return useAttribs[key] = getIdReplacement(loaderContext, options, useAttribs[key]);
+        }
+        if (key.toLowerCase().indexOf('href') !== -1) {
+          return useAttribs[key] = useAttribs[key].replace(/^#([a-zA-Z0-9]+)/, (_, $1) =>
+            '#' + getIdReplacement(loaderContext, options, $1)
+          );
+        }
+        if (typeof useAttribs[key] === 'string') {
+          return useAttribs[key] = useAttribs[key].replace(/url\(#([^\)]+)\)/, (_, $1) =>
+            'url(#' + getIdReplacement(loaderContext, options, $1) + ')'
+          );
+        }
+      })
+    }
+
+    let attribs = JSON.stringify(useAttribs);
+    if (options.cssModules) {
+
+      attribs = attribs.replace(/"class":"([^"]*)"/, (_, $1) => {
+        const classes = $1.split(/\s+/).map(className => {
+          if (!className) {
+            return '';
+          }
+          const string = JSON.stringify(className);
+          return `styles && styles[${string}] || ${string}`;
+        });
+        return `"class":[${classes.join(',')}].join(' ')`;
+      });
+    }
 
     let children = '[]';
     if (node.children) {
         children = '[' +
             node.children
-                .map(childNode => assembleNode(childNode))
+                .map(childNode => assembleNode(loaderContext, childNode))
                 .join(', ') +
         ']';
+    }
+
+    if (root) {
+      return `h('${node.name}', Object.assign(${attribs}, rest), ${children})`;
     }
 
     return `h('${node.name}', ${attribs}, ${children})`
 }
 
-module.exports = function(source) {
+module.exports = function(source, map) {
+    if (this.cacheable) this.cacheable();
+
     const handler = new htmlparser.DefaultHandler(function (error, dom) {});
     const parser = new htmlparser.Parser(handler);
     parser.parseComplete(source);
@@ -30,13 +86,17 @@ module.exports = function(source) {
         throw new Error('Could not find svg element');
     }
 
-    const svg = assembleNode(svgNode);
+    const svg = assembleNode(this, svgNode, true);
 
     this.callback(null, `
 
 import { h } from 'preact';
 
-export default function () {
+export default function (props) {
+    const styles = props.styles;
+    const rest = Object.assign({}, props);
+    delete rest.styles;
+
     return ${svg};
 };
 
---
+++ test/helpers.js
@@ -0,0 +1,56 @@
+const svgLoader = require('../');
+exports.svgLoader = svgLoader;
+
+function runLoader(loader, filename, input, map, addOptions, callback) {
+	var opt = {
+		options: {
+			context: ""
+		},
+		callback: callback,
+		async: function() {
+			return callback;
+		},
+		loaders: [{request: "/path/preact-svg-loader"}],
+		loaderIndex: 0,
+		context: "",
+		resource: filename,
+		resourcePath: filename,
+		request: "preact-svg-loader!"+filename,
+		emitError: function(message) {
+			throw new Error(message);
+		}
+	};
+	Object.keys(addOptions).forEach(function(key) {
+		opt[key] = addOptions[key];
+	});
+	loader.call(opt, input, map);
+}
+
+exports.test = (name, filename, input, options, callback) => {
+  it(name, done => {
+    runLoader(svgLoader, filename, input, null, options, function(err, data) {
+      if (err) throw err;
+      callback(data);
+      done();
+    })
+  });
+};
+
+exports.testVNodes = (name, filename, input, options, callback) => {
+  exports.test(name, filename, input, options, parsed => {
+    const runnable = parsed.replace(/^[^]*(function[^]*\});[^]*$/, (_, $1) => $1);
+    const vnodes = [];
+    const h = (nodeName, attributes, children) => {
+      vnodes.push({nodeName, attributes, children})
+      return vnodes[vnodes.length - 1];
+    };
+    const props = options.props || {};
+    try {
+      eval(`(${runnable})(props)`);
+    } catch(e) {
+      console.error('Error testing', runnable);
+      throw e;
+    }
+    callback(vnodes);
+  });
+};
